f65ef1bd34f9514d1dcde751b1d5d48c87303c99
cyberbudy <cyberbudy@gmail.com>
2022-01-08 23:08:31 +0200

    Fix new attribute exception

--- jet/utils.py
+++ jet/utils.py
@@ -265,6 +265,12 @@ def get_model_queryset(admin_site, model, request, preserved_filters=None):
         pass
 
     try:
+        change_list_args.append(model_admin.search_help_text)
+    except AttributeError:
+        # django version < 4.0
+        pass
+
+    try:
         cl = ChangeList(*change_list_args)
         queryset = cl.get_queryset(request)
     except IncorrectLookupParameters:
