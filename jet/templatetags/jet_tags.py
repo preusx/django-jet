52283df034869714f565cf41640c17ac08f3bf01
Alex Tkachenko <preusx.dev@gmail.com>
2023-09-15 16:24:06 +0300

    Fixed jet to work with django 4 template changes

--- jet/templatetags/jet_tags.py
+++ jet/templatetags/jet_tags.py
@@ -14,7 +14,7 @@ from django.contrib.admin.widgets import RelatedFieldWidgetWrapper
 from django.utils.formats import get_format
 from django.utils.safestring import mark_safe
 from django.utils.encoding import smart_str
-from jet import settings, VERSION
+from .. import settings
 from jet.models import Bookmark
 from jet.utils import get_model_instance_label, get_model_queryset, get_possible_language_codes, \
     get_admin_site, get_menu_items
@@ -23,6 +23,9 @@ try:
     from urllib.parse import parse_qsl
 except ImportError:
     from urlparse import parse_qsl
+
+
+VERSION = '1.0.10'
 
 
 register = template.Library()
