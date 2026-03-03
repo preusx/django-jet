992c56c473d158525d376a2e1c3d7703f0446088
Eugene Kurilenko <eighthcolor22@gmail.com>
2026-03-03 12:11:17 +0200

    Fix choices

--- jet/filters.py
+++ jet/filters.py
@@ -25,28 +25,30 @@ class RelatedFieldAjaxListFilter(RelatedFieldListFilter):
         return True
 
     def field_choices(self, field, request, model_admin):
-        model = field.remote_field.model if hasattr(field, 'remote_field') else field.related_field.model
+        model = (
+            field.remote_field.model
+            if hasattr(field, 'remote_field')
+            else field.related_field.model
+        )
         app_label = model._meta.app_label
         model_name = model._meta.object_name
 
-        self.ajax_attrs = format_html('{0}', flatatt({
-            'data-app-label': app_label,
-            'data-model': model_name,
-            'data-ajax--url': reverse('jet:model_lookup'),
-            'data-queryset--lookup': self.lookup_kwarg
-        }))
+        self.ajax_attrs = format_html(
+            '{0}',
+            flatatt(
+                {
+                    'data-app-label': app_label,
+                    'data-model': model_name,
+                    'data-ajax--url': reverse('jet:model_lookup'),
+                    'data-queryset--lookup': self.lookup_kwarg,
+                }
+            ),
+        )
 
         if self.lookup_val is None:
             return []
 
-        other_model = get_model_from_relation(field)
-        if hasattr(field, 'rel'):
-            rel_name = field.rel.get_related_field().name
-        else:
-            rel_name = other_model._meta.pk.name
-
-        queryset = model._default_manager.filter(**{rel_name: self.lookup_val}).all()
-        return [(x._get_pk_val(), smart_str(x)) for x in queryset]
+        return RelatedFieldListFilter.field_choices(self, field, request, model_admin)
 
 
 try:
